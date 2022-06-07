ff4ac9dc5882463c890636d945c587404a851a47
matiasmiodosky <matias.miodosky@emilabs.ai>
2022-06-07 11:56:00 -0300

    fixed test

--- src/test/java/com/example/demodatadog/DemoDataDogApplicationTests.java
+++ src/test/java/com/example/demodatadog/DemoDataDogApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.demodatadog;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoDataDogApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
